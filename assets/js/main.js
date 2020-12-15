ec4461c28031d4cb15575c7cec7881b2aa22d248
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-15 13:58:56 +0100

    Creato il file dell'esercizio
    Creato il file dell'esercizio e collegati css e js

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Istruzioni:
+// Ricreare lo slider di immagini ma questa volta con Vue
+// Bonus ufficiale:
+// Facciamo in modo che il nostro slider scorra automaticamente in avanti,
+// e ogni 3 secondi visualizzi un immagine differente.
+// Tip: possiamo usare una lifecycle Hook per inserire un nostro script quando l'istanza Vue é stata caricata.
+// Possibili extra Bonus:
+// cliccando sui pallini visualizzeremo l'immagine corrispondente
+// cliccando sulle frecce della tastiera sinista e destra scorriamo tra le immagini
+// I bonus metteteli in una cartella "bonus".
+
+let app = new Vue({
+    el: "#app",   
+});
